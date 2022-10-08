0e207b7c449d540b6716abf95206ab1a01a6564b
CristianAtienza27 <cristianatienza26@gmail.com>
2022-10-08 18:23:15 +0200

    1 y 2 ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,19 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
+  courses = COURSES;
 
+  title = COURSES[0].description;
 
+  course = COURSES[0];
+
+  price = 9.99;
+
+  rate = 0.67;
+
+  startDate = new Date(2000, 0, 1);
+
+  onCourseSelected(course:Course){
+    console.log("App component - click event bubbled ...", course);
+  }
 }
